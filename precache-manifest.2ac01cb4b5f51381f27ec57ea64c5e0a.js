639da322348469c67abe072a5bae29928387aac7
Anoop Lath <Anoop.Lath@idc.bedbath.com>
2019-11-08 17:12:18 +0530

    Updates

---
+++ precache-manifest.2ac01cb4b5f51381f27ec57ea64c5e0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8db5ef755abb37f8e8081fec16d49b",
+    "url": "/campaign-manager/index.html"
+  },
+  {
+    "revision": "5fbc93edfcbd9409ee19",
+    "url": "/campaign-manager/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "37842adfbbe533f0ef88",
+    "url": "/campaign-manager/static/css/main.907e3009.chunk.css"
+  },
+  {
+    "revision": "5fbc93edfcbd9409ee19",
+    "url": "/campaign-manager/static/js/2.dc57decf.chunk.js"
+  },
+  {
+    "revision": "37842adfbbe533f0ef88",
+    "url": "/campaign-manager/static/js/main.d9ab8b19.chunk.js"
+  },
+  {
+    "revision": "b3d2818a431823022cb6",
+    "url": "/campaign-manager/static/js/runtime-main.ed930381.js"
+  },
+  {
+    "revision": "977ecf7be74437c437106e00ad13f379",
+    "url": "/campaign-manager/static/media/Bitmap.977ecf7b.png"
+  }
+]);
