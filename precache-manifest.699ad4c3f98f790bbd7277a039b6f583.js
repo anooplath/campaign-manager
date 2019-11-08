c76aa6446806d33167106fc2941d12a2252e4c91
Anoop Lath <Anoop.Lath@idc.bedbath.com>
2019-11-08 17:13:44 +0530

    Updates

---
+++ precache-manifest.699ad4c3f98f790bbd7277a039b6f583.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ba7980a53e16d1fafb341e6bc788dc",
+    "url": "/campaign-manager-website/index.html"
+  },
+  {
+    "revision": "5fbc93edfcbd9409ee19",
+    "url": "/campaign-manager-website/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "e6e29a447ce96b531841",
+    "url": "/campaign-manager-website/static/css/main.907e3009.chunk.css"
+  },
+  {
+    "revision": "5fbc93edfcbd9409ee19",
+    "url": "/campaign-manager-website/static/js/2.dc57decf.chunk.js"
+  },
+  {
+    "revision": "e6e29a447ce96b531841",
+    "url": "/campaign-manager-website/static/js/main.7d9e9fd3.chunk.js"
+  },
+  {
+    "revision": "f30388de8310f7213a5b",
+    "url": "/campaign-manager-website/static/js/runtime-main.2faec4bc.js"
+  },
+  {
+    "revision": "977ecf7be74437c437106e00ad13f379",
+    "url": "/campaign-manager-website/static/media/Bitmap.977ecf7b.png"
+  }
+]);
